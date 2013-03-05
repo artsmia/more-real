42fae2f24e9cae556d458f6aaf72f816755a2afc
Kjell <kjell@leanside.com>
2013-03-05 16:36:02 -0600

    More real treatment

---
+++ signput.php
@@ -0,0 +1,25 @@
+<?php
+  phpinfo();
+  exit;
+
+$S3_KEY='AKIAJMMIBUNQUOYAZYFQ';
+$S3_SECRET='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';
+$S3_BUCKET='/more-real-tabloid'; // Make sure to leave the / on the front of the bucket here
+
+$EXPIRE_TIME=(60 * 5); // 5 minutes
+$S3_URL='http://s3.amazonaws.com';
+
+$objectName='/' . $_GET['name'];
+
+$mimeType=$_GET['type'];
+$expires = time() + $EXPIRE_TIME;
+$amzHeaders= "x-amz-acl:public-read";
+$stringToSign = "PUT\n\n$mimeType\n$expires\n$amzHeaders\n$S3_BUCKET$objectName";
+$sig = urlencode(base64_encode(hash_hmac('sha1', $stringToSign, $S3_SECRET, true)));
+
+$url = urlencode("$S3_URL$S3_BUCKET$objectName?AWSAccessKeyId=$S3_KEY&Expires=$expires&Signature=$sig");
+
+echo $url;
+
+/* https://github.com/carsonmcdonald/direct-browser-s3-upload-example/blob/master/signput.php */
+?>
