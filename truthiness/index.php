6865664a58e73cbc3041c57cd6f3778140c9519a
Kjell <kjell@leanside.com>
2013-03-19 09:34:33 -0500

    Style for the truthiness pages

--- truthiness/index.php
+++ truthiness/index.php
@@ -1,16 +1,35 @@
 <html>
   <head>
+    <link rel="stylesheet" href="/more-real/css/reset.css">
+    <link rel="stylesheet" href="/more-real/css/morereal.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/3.0.2/css/font-awesome.min.css">
+    <script type="text/javascript" src="/more-real/js/modernizr.custom.js"></script>
+    <script type="text/javascript" src="/more-real/js/css3-mediaqueries.js"></script>
+    <script type="text/javascript" src="http://use.typekit.com/vft0zxk.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <script type="text/javascript" src="/more-real/js/foutbgone.js" ></script>
+    <script type="text/javascript">fbg.hideFOUT('asap', 400);</script>
+    <link rel="stylesheet" href="/more-real/css/tabloid.css">
+
     <title>Truthiness! - More Real - Minneapolis Institute of Arts</title>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.firebase.com/v0/firebase.js"></script>
   </head>
-  <style>
-    #gallery figure { width: 20%; float: left }
-    #gallery figure img { max-width: 100%; }
-  </style>
   <body>
+    <aside>
+      <img src="/more-real/images/MR-title.png" alt="More Real? Art in the Age of Truthiness" />
+      <nav class="row clearfix" id="nav">
+        <ul class="navL">
+          <li><a href="/more-real/index.html#about">About the Exhibition</a></li>
+          <li><a href="/more-real/index.html#tabloid">Write your own headline</a></li>
+        </ul>
+      </nav>
+      <h1>Write your own reality</h1>
+      <p>What is real? How do you define real? If you're talking about what you can feel, what you can smell, what you can taste and see, then real is simply electrical signals interpreted by your brain. This is the world that you know. The world as it was at the end of the twentieth century. It exists now only as part of a neural-interactive simulation that we call the Matrix.</p>
+    </aside>
     <section id="gallery">
     </section>
+
     <script>
     function add_tabloid(snap) {
       if(snap.val() == null || snap.val().cover == undefined) { // don't show until the image has been uploaded
